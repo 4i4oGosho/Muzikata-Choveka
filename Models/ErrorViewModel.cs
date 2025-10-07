ea975c505a4a46ceb5be5b345988e3c2f7aae8dd
Genataa <ganchevevgeni@abv.bg>
2025-10-07 14:49:37 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Muzikata_Choveka.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
